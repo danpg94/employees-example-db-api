09a47924cc59f3eed482b74292e28c386ccaabb4
Daniel Palma <danpalma94@gmail.com>
2025-03-18 23:14:00 +0000

    restructured sqlalchemy-connect-test.py

--- sqlalchemy-connect-test.py
+++ sqlalchemy-connect-test.py
@@ -6,11 +6,12 @@ load_dotenv()
 MYSQL_USER = getenv('MYSQL_USER')
 MYSQL_PSWD = getenv('MYSQL_PSWD')
 MYSQL_IP = getenv('MYSQL_IP')
-engine = create_engine(f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PSWD}@{MYSQL_IP}", echo=True)
+DB_NAME= getenv('DB_NAME')
 
-connection = engine.connect()
-query = text("SELECT * FROM employees.employees LIMIT 10")
-result = connection.execute(query)
+engine = create_engine(f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PSWD}@{MYSQL_IP}/{DB_NAME}")
 
-for row in result:
-    print(row)
+with engine.connect() as conn:
+    query = text("SELECT * FROM employees LIMIT 10")
+    result = conn.execute(query)
+    for row in result:
+        print(row)
